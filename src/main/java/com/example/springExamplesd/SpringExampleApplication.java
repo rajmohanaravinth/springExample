f10855c48aef1535b3e7bcc5e8087871d08f0f5b
Rajmohan Aravinth <Rajmohan.aravinth@gmail.com>
2020-02-23 12:00:36 +0530

    test

---
+++ src/main/java/com/example/springExamplesd/SpringExampleApplication.java
@@ -0,0 +1,33 @@
+package com.example.springExamplesd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+
+	
+	@RequestMapping(value = "/id", params = "id", method = RequestMethod.GET)
+	@ResponseBody
+	public String getBarBySimplePathWithExplicitRequestParam(
+	  @RequestParam("id") long id) {
+	    return "Get a specific Bar with id=" + id;
+	}
+	
+	 @RequestMapping(value = "/personId")              
+	  String getId(@RequestParam String personId){
+	    System.out.println("ID is "+personId);  
+	    return "Get ID from query string of URL without value element";  
+	  }
+
+}
